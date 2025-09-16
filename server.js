1e1cb001f82125f62512eeecf3cb45d2013990e9
roldanhilda49-design <roldanhilda49@gmail.com>
2025-09-15 23:00:31 -0400

    Preparado para Render

--- server.js
+++ server.js
@@ -1,55 +1,51 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const port = 3000;
+// Puerto dinámico para Render
+const port = process.env.PORT || 3000;
 
-// Servir archivos estáticos
-app.use(express.static(__dirname + "/public"));
+// Servir archivos estáticos desde /public
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Ruta principal
 app.get('/', (req, res) => {
-   res.sendFile(__dirname + "/public/index.html");
+   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-server.listen(port, () => {
-  console.log(`Servidor Express escuchando en http://localhost:${port}`);
-});
-
-// Guardamos usuarios
+// Guardar usuarios conectados
 let usuarios = {};
 
 io.on("connection", (socket) => {
-   console.log("Un cliente conectado:", socket.id);
+   console.log("Cliente conectado:", socket.id);
 
    // Registrar usuario
    socket.on("registrar_usuario", (nombre) => {
        usuarios[socket.id] = nombre;
-       console.log(`Usuario registrado: ${nombre} (${socket.id})`);
-
-       // Avisar a todos
        io.emit("mensaje", `${nombre} se ha conectado`);
        io.emit("lista_usuarios", Object.values(usuarios));
    });
 
    // Chat general
    socket.on("mensaje", (mensaje) => {
-        const nombre = usuarios[socket.id] || "Anónimo";
-        io.emit("mensaje", `${nombre}: ${mensaje}`);
+       const nombre = usuarios[socket.id] || "Anónimo";
+       io.emit("mensaje", `${nombre}: ${mensaje}`);
    });
 
    // Chat privado
    socket.on("mensaje_privado", ({para, mensaje}) => {
-        const nombre = usuarios[socket.id] || "Anónimo";
-        const idDestino = Object.keys(usuarios).find(key => usuarios[key] === para);
+       const nombre = usuarios[socket.id] || "Anónimo";
+       const idDestino = Object.keys(usuarios).find(key => usuarios[key] === para);
 
-        if(idDestino){
-            io.to(idDestino).emit("mensaje_privado", {de: nombre, mensaje});
-            socket.emit("mensaje_privado", {de: "Tú", mensaje: mensaje});
-        }
+       if(idDestino){
+           io.to(idDestino).emit("mensaje_privado", {de: nombre, mensaje});
+           socket.emit("mensaje_privado", {de: "Tú", mensaje});
+       }
    });
 
    // Desconexión
@@ -59,4 +55,9 @@ io.on("connection", (socket) => {
        delete usuarios[socket.id];
        io.emit("lista_usuarios", Object.values(usuarios));
    });
+});
+
+// Escuchar puerto
+server.listen(port, () => {
+   console.log(`Servidor escuchando en el puerto ${port}`);
 });
